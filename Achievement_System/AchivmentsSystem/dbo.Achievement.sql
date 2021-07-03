f18fc3db6ad1400ea752b11fd691bb593b4bd6ee
ShinnikovVA <mr.shchinnikov@mail.ru>
2021-07-03 19:12:23 +0400

    загрузил 3 файла создания таблиц бд может не работать, если качать с гита

---
+++ Achievement_System/AchivmentsSystem/dbo.Achievement.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Achievement] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Наименование] NVARCHAR (50)  NOT NULL,
+    [Описание]     NVARCHAR (MAX) NULL,
+    [Кол-во очков] INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
