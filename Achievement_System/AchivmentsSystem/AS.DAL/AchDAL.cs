02a79c3396940fcab8c459eb42558464eb4c30b2
ShinnikovVA <mr.shchinnikov@mail.ru>
2021-07-06 20:58:09 +0400

    создал несколько простых классов

---
+++ Achievement_System/AchivmentsSystem/AS.DAL/AchDAL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.DAL
+{
+    class AchDAL
+    {
+        public const string _FILES_PATCH = @"F:\Рабочее пространство\Парктика 2021\Jule2021\Achievement_System\AchivmentsSystem";
+        public void AddAchievement()
+        {
+
+        }
+        public void RemoveAchievement(Guid id)
+        {
+
+        }
+        public void EditAchievement(Guid id, string name, string text)
+        {
+            if(name == null)
+            {
+
+            }
+            if(text == null)
+            {
+
+            }
+        }
+    }
+}
---
+++ Achievement_System/AchivmentsSystem/AS.Entities/Achievement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.Entities
+{
+    public class Achievement
+    {
+        public Achievement( string name, string text)
+        {
+            ID = Guid.NewGuid();
+            Name = name;
+            Text = text;
+        }
+
+        public Guid ID { get; }
+        public string Name { get; private set; }
+        public string Text { get; private set; }
+    }
+}
