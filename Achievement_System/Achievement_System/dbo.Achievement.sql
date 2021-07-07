d61593ce3dfd503281965d9ff2b21d6c423a7a79
ShinnikovVA <mr.shchinnikov@mail.ru>
2021-07-08 02:43:00 +0400

    После удаления ненужного проекта

--- Achievement_System/Achievement_System/dbo.Achievement.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Achievement]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Наименование] NVARCHAR(50) NOT NULL, 
-    [Описание] NVARCHAR(MAX) NULL, 
-    [Кол-во очков] INT NULL
-)
